4cb0c2dd60ca4b5938ecb0a84748d7c2180f1b22
SEREINO <kouamenock00@gmail.com>
2022-04-07 19:44:18 -0700

    Last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,9 @@
 #include <unistd.h>
 
 /**
- *main - prints given text using write()  
- *Return: 1 if successful
- */
+* main - prints given text using write()  
+* Return: 1 if successful
+*/
 
 int main(void)
 {
